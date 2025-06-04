bd6f4525a719753b9767ab91e13f6ce4df9c07a5
Srirag T <tsrirag@gmail.com>
2025-06-05 00:12:35 +0530

    Updated

--- Hashing/CharacterHashing.java
+++ Hashing/CharacterHashing.java
@@ -2,12 +2,19 @@ package Hashing;
 import java.util.*;
 public class CharacterHashing {
     public static void main(String[] args){
-        String str = "abcdabefc";
-        int[] hash = new int[25];
-        for(int i=0;i<str.length();i++){
-         int chNum = (int) str.charAt(i);
-         hash[chNum-i] +=1;
+        Scanner sc = new Scanner(System.in);
+        int[] hash = new int[26];
+        String str = sc.nextLine();
 
+        for(int i=0;i<str.length();i++){
+         hash[str.charAt(i)-'a'] +=1;
         }
+        int t = sc.nextInt();
+        while(t!=0){
+            char ch = sc.next().charAt(0);
+            System.out.println(hash[ch - 'a']);
+            t--;
+        }
+
     }
 }
