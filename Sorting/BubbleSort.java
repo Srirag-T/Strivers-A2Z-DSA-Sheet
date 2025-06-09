cbd8e27d0058f3e137264cd388c009c46978b317
Srirag T <tsrirag@gmail.com>
2025-06-09 22:36:46 +0530

    Updated

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class BubbleSort {
     public static void Bubblesort(int arr[], int n){
-       for(int i=0;i<n;i++){
-           for(int j=0;j<i;j++){
-               if(arr[j]<arr[j+1]){
+       for(int i=n-1;i>=1;i--){
+           for(int j=0;j<=i-1;j++){
+               if(arr[j]>arr[j+1]){
                   int temp = arr[j];
                   arr[j] = arr[j+1];
                   arr[j+1] = temp;
