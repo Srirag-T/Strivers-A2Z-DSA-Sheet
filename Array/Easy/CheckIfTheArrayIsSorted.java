1ed8817a83989d00a31aba34274ff880249e675c
Srirag T <tsrirag@gmail.com>
2025-07-08 21:26:09 +0530

    Updated

--- Array/Easy/CheckIfTheArrayIsSorted.java
+++ Array/Easy/CheckIfTheArrayIsSorted.java
@@ -2,7 +2,7 @@ package Array.Easy;
 import java.util.*;
 public interface CheckIfTheArrayIsSorted {
     static boolean check(int arr[], int n) {
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 1; i < n ; i++) {
             if (arr[i] >=arr[i - 1] ) {
             }
             else{
